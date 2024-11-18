d11a36823222b4643c6765ee3cf237bbe75986d9
zoufi <zoufi@zoufaond>
2024-11-18 17:11:24 +0100

    simulations

--- Python_optimal_control/optyEngine.py
+++ Python_optimal_control/optyEngine.py
@@ -107,10 +107,11 @@ def create_trajectory(num_nodes, duration, interval_value):
     d_trajectory = np.array([])
     time = np.linspace(0.0, duration, num=num_nodes)
     np.random.seed(5)
-    weight = np.random.randn(6)*0.4
+    # weight = np.random.randn(6)*0.4
+    weight = np.ones(6)*0.3
     # weight = [1.5,1.2,0.2,1.1,2.1,1]
     for i in range(6):
-        icos = (-np.cos(time*np.pi)+1)*weight[i]
+        icos = (-np.cos(time*np.pi)+1)*weight[i]-0.11
         trajectory = np.append(trajectory,icos)
         d_icos = np.concatenate((np.zeros(1),np.diff(icos)/interval_value))
         d_trajectory = np.append(d_trajectory,d_icos)
